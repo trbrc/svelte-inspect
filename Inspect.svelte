3ef9addb88a7c79cb4c6135dbcc1885e97ecdd5e
tor <tor.bruce@gmail.com>
2019-06-02 13:32:21 +0200

    NPM init and basic component

---
+++ Inspect.svelte
@@ -0,0 +1,21 @@
+<script>
+  $: propKeys = Object.keys($$props);
+</script>
+
+{#each propKeys as key}
+  <pre class=inspect>{key} = {Object.prototype.toString.call($$props[key])};</pre>
+{/each}
+
+<style>
+  .inspect {
+    font-family:
+      Menlo,
+      Consolas,
+      Lucida Console,
+      Courier New,
+      Dejavu Sans Mono,
+      monospace;
+    font-size: 11px;
+    line-height: 1.35;
+  }
+</style>
